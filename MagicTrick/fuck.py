a7d1dc30f2348fd42d4cfde661fe706b26d2c919
sh-zh-7 <2431297348@qq.com>
2020-05-11 21:05:20 +0800

    Reformat the ml part and and magic trick

---
+++ MagicTrick/fuck.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+
+def get_prediction(df1: pd.DataFrame, df2: pd.DataFrame):
+    """
+    三行代码实现预测
+    :param df1: 带有ID的data frame, 最后的prediction顺序和这个ID的顺序一致
+    :param df2: 带有纳税总额的data frame, 只要根据三年的综合是否为0即可进行判断
+    :return: 预测的结果
+    """
+    df2["纳税总额"].fillna(0, inplace=True)
+    return [1 if sum(df2[row["ID"] == df2["ID"]]["纳税总额"]) == 0 else 0
+            for index, row in df1.iterrows()]
+
+
+def get_accuracy(ground_truth, prediction):
+    """
+    跳过ground_truth中的nan项，做一个准确率和false positive的计算
+    :param ground_truth: 真实值，可能带有nan
+    :param prediction: 预测值
+    :return: false positive的数量和准确率
+    """
+    num_true_positive = 0
+    num_false_positive = 0
+    
+    skip_nan_length = 0
+    for index in range(len(prediction)):
+        if not pd.isna(ground_truth[index]):
+            skip_nan_length += 1
+            if prediction[index] == ground_truth[index]:
+                num_true_positive += 1
+            else:
+                num_false_positive += 1
+    return num_true_positive / skip_nan_length, num_false_positive
+
+def main(filename1, filename2):
+    data_set1 = pd.read_csv(filename1, encoding="GBK")
+    data_set2 = pd.read_csv(filename2, encoding="GBK")
+    ground_truth = data_set1["flag"]
+
+    prediction = get_prediction(data_set1, data_set2)
+    accuracy, false_positive_num = get_accuracy(ground_truth, prediction)
+    print("#False Positive: %d" % false_positive_num)
+    print("Accuracy: %.2f%%" % (accuracy * 100))
+            
+
+if __name__ == "__main__":
+    # 得到train set和test set的false positive和accuracy
+    print("Train Set:")
+    main("base_train_sum.csv", "year_report_train_sum.csv")
+    print("--------------------------------------------")
+    print("Validation Set:")
+    main("base_verify1.csv", "year_report_verify1.csv")
---
+++ TraditionML/data/spliter.py
@@ -0,0 +1,44 @@
+"""
+    这个:文件的作用就是把已经处理好的data_set.csv
+    按照8:2的比例重新分为train_set.csv和test_set.csv
+    理由如下：
+        1. 如果直接在程序中使用train_test_split，那么会增加程序运行的时间，降低开发效率。
+        2. 从物理上直接把文件分成两个文件，能有效避免data leakage，避免我们的估计过于乐观
+"""
+
+import random
+import pandas as pd
+from src import utils
+import logging
+
+SPILT_RATE = 0.2
+
+def GetDataSet(filename: str):
+    """ 读取整一个data_set文件，注意这个文件时utf-8编码格式的 """
+    return pd.read_csv(filename, encoding="utf-8")
+
+def GetRowCount(data_set: pd.DataFrame):
+    """ 获取data_set的行数 """
+    return len(data_set)
+
+def GetRandomNumber(n: int):
+    """ 从[0, n)范围中按照split_rate划分 """
+    all_rows = range(n)
+    test_rows = random.sample(range(n), int(n * SPILT_RATE))
+    train_rows = [row for row in all_rows if row not in test_rows]
+    return train_rows, test_rows
+
+def ExportTrainAndTestSet(data_set: pd.DataFrame, train_rows, test_rows):
+    """ 根据给定的训练集和测试集行数导出对应的train_set和test_set """
+    data_set.iloc[train_rows].to_csv("train_set.csv", index=False)
+    data_set.iloc[test_rows].to_csv("test_set.csv", index=False)
+
+
+if __name__ == "__main__":
+    utils.SetLogger("../log")
+    data_set = GetDataSet("./data_set.csv")
+    line_count = GetRowCount(data_set)
+    train_lines, test_lines = GetRandomNumber(line_count)
+    logging.info("Export train set and test set...")
+    ExportTrainAndTestSet(data_set, train_lines, test_lines)
+    logging.info("Done!")
